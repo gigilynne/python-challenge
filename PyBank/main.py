687056f1002ba9580007a5803667c93a9ac20255
Jennifer Gaddie <jennifergaddie@Jennifers-MacBook-Pro.local>
2021-04-03 19:32:22 -0400

    updating code in main.py for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,19 @@
 #main.py
 
-#extra file
+## PyBank
+
+#import modules
+import os
+import csv
+
+#set a path for the csv file using the join for both windows and mac os
+budget_data_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+
+#open and then read the csv
+with open(budget_data_csv) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+
+    #read the header row because they exist
+    csv_header = next(csv_file)
+    print(f"Header: {csv_header}")
+
