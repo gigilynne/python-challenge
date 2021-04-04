3ad1d6803b691d8a2da78c6e2a07e6f358db0062
Jennifer Gaddie <jennifergaddie@Jennifers-MacBook-Pro.local>
2021-04-04 16:46:48 -0400

    update with latest work to code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,40 +1,31 @@
 #main.py
-
 ## PyBank
 
 #import modules
 import os
 import csv
+from datetime import datetime
 
 #set a path for the csv file using the join for both windows and mac os
 budget_data_csv = os.path.join('Resources', 'budget_data.csv')
 
 #open and then read the csv - attemped to only open as csv, but will move to open directly into reading as a dictionary file
-#with open(budget_data_csv) as csv_file:
- #   csv_reader = csv.reader(csv_file, delimiter=",")
+with open(budget_data_csv) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
 
     #read the header row because they exist
- #   csv_header = next(csv_file)
- #   print(f"Header: {csv_header}")
+    csv_header = next(csv_file)
+    print(f"Header: {csv_header}")
 
-#create a dictionary from the csv
-with open('budget_data.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            total = 0.0
-            for number in numbers:
-                total += number
-            print(f'Total {": ".join(row)}')
-            line_count += 1
-        print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
-        line_count += 1
-    print(f'Processed {line_count} lines.')
+ #need to interpret the details in the csv for analysis
+ data = []
+ for row in csv_reader
+ date = datetime.strptime(row[0], '%m/%YYYY')
+ Profit/Losses = int(row[1])
+
 
 # Write a function that returns the arithmetic sum for a list of numbers
-numbers = [(Profit/Losses)]
-    sum = sum(numbers)
+    sum = sum(Profit/Losses)
     print(f'Total Months:' sum)
 
 
@@ -43,25 +34,29 @@ numbers = [(Profit/Losses)]
 
 
 # Specify the file to write to
-output_path = os.path.join("..", "Analysis", "new.csv")
+output_path = os.path.join(Analysis", "analysis.txt")
 
-# Open the file using "write" mode. Specify the variable to hold the contents
-with open(output_path, 'w', newline='') as csvfile:
-
-    # Initialize csv.writer
-    csvwriter = csv.writer(csvfile, delimiter=',')
+# Open the file using "write" mode. Specify the variable to hold the contents and open the file to write
+with open(output_path,"analysis.txt", 'w',):
+    f = open(analysis.txt)
 
     #Set the Header row for the table analysis
-    csvwriter.writerow(['Financial Analysis'])
-    
+    f.write("Financial Analysis")
+    f.write("------------------------------")
+
     # Write the first data row - pass the sum of the total of the total months into the written file
-    csvwriter.writerow(['Total Months': "x"])
+    f.write("Total Months:", "x"])
 
     # Write the second row - pass the net total amount of the "Profit/Losses" over the entire period into the written file
-    csvwriter.writerow(['Total':"X"])
+    f.write("Total:", "x")
 
-    # Write the third row - pass the net total amount of the Greatest Increase in Profits over the entire period into the written file
-    csvwriter.writerow(['Total':"X"])
+    # Write the third row - pass the changes in Profits over the entire period into the written file
+    f.write("Average Change:", "x")
 
-    # Write the fourth row - pass the net total amount of the Greatest Decrease in Profits over the entire period into the written file
-    csvwriter.writerow(['Total':"X"])
+    # Write the fourth row - pass the net total amount of the Greatest Increase in Profits over the entire period into the written file
+    f.write("Greatest Increase in Profits:", "x", "x")
+
+    # Write the fifth row - pass the net total amount of the Greatest Decrease in Profits over the entire period into the written file
+    f.write("Greatest Decrease in Profits:", "x", "x")
+    f.close()
+
--- PyPoll/main2.py
+++ PyPoll/main2.py
@@ -1,4 +1,22 @@
 #main1.py
 
-#jen's python file
+## PyPoll
+
+#import modules
+import os
+import csv
+
+print(dir(csv))
+
+#set a path for the csv file using the join for both windows and mac os
+#budget_data_csv = os.path.join('Resources', 'election_data.csv')
+
+#open and then read the csv - attemped to only open as csv, but will move to open directly into reading as a dictionary file
+#with open(budget_data_csv) as csv_file:
+ #   csv_reader = csv.reader(csv_file, delimiter=",")
+
+    #read the header row because they exist
+  #  csv_header = next(csv_file)
+  #  print(f"Header: {csv_header}")
+
 
