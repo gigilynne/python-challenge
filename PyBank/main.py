b6460a317212c86959873051e5efcb46cf5f9a99
Jennifer Gaddie <jennifergaddie@Jennifers-MacBook-Pro.local>
2021-04-06 09:28:03 -0400

    updating with changes to code

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,7 +22,6 @@ with open(budget_data_csv) as csv_file:
     print(f"Header: {csv_header}")
 
 #need to interpret the details in the csv for analysis and return the totals for months, total profits, change in profits by accreting each through a for loop
-
     for row in csv_reader:
         months += 1
         profits += int(row[1])
@@ -33,34 +32,38 @@ with open(budget_data_csv) as csv_file:
     print(profits/months)
 
 #Return the highest number in (row[1]) and zip to (row[0]) 
-
+    #for x in range (row[1])
+    increase = max(row[1])
+    print(increase)
 
 #Return the lowest number in (row[1]) and zip to (row[0]) 
+    decrease = min(row[1])
+    print(decrease)
 
-# # Specify the file to write to
-# output_path = os.path.join(Analysis", "analysis.txt")
+# Specify the file to write to
+output_path = os.path.join('Analysis', "analysis.txt")
 
-# # Open the file using "write" mode. Specify the variable to hold the contents and open the file to write
-# with open(output_path,"analysis.txt", 'w',):
-#     f = open(analysis.txt)
+# Open the file using "write" mode. Specify the variable to hold the contents and open the file to write
+with open(output_path,"analysis.txt", 'w'):
+    f = open(analysis.txt)
 
-#     #Set the Header row for the table analysis
-#     f.write("Financial Analysis")
-#     f.write("------------------------------")
+    #Set the Header row for the table analysis
+    f.analysis.write("Financial Analysis")
+    f.analysis.write("------------------------------")
 
-#     # Write the first data row - pass the sum of the total of the total months into the written file
-#     f.write("Total Months:", months)
+    # Write the first data row - pass the sum of the total of the total months into the written file
+    f.analysis.write("Total Months:", months)
 
-#     # Write the second row - pass the net total amount of the "Profit/Losses" over the entire period into the written file
-#     f.write("Total: $", profits)
+    # # Write the second row - pass the net total amount of the "Profit/Losses" over the entire period into the written file
+    # f.write("Total: $", profits)
 
-#     # Write the third row - pass the changes in Profits over the entire period into the written file
-#     f.write("Average Change: $", profits/months)
+    # # Write the third row - pass the changes in Profits over the entire period into the written file
+    # f.write("Average Change: $", profits/months)
 
-#     # Write the fourth row - pass the net total amount of the Greatest Increase in Profits over the entire period into the written file
-#     f.write("Greatest Increase in Profits:", "x", "x")
+    # # Write the fourth row - pass the net total amount of the Greatest Increase in Profits over the entire period into the written file
+    # f.write("Greatest Increase in Profits:", increase)
 
-#     # Write the fifth row - pass the net total amount of the Greatest Decrease in Profits over the entire period into the written file
-#     f.write("Greatest Decrease in Profits:", "x", "x")
-#     f.close()
+    # # Write the fifth row - pass the net total amount of the Greatest Decrease in Profits over the entire period into the written file
+    # f.write("Greatest Decrease in Profits:", decrease)
+    # f.close()
 
