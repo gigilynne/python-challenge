91c7faff76e15f83db460b5eef62c963719a0347
Jennifer Gaddie <jennifergaddie@Jennifers-MacBook-Pro.local>
2021-04-06 23:09:12 -0400

    updating with code changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,7 +11,7 @@ budget_data_csv = os.path.join('Resources', 'budget_data.csv')
 #set all variables to zero before opening the files so they are ready to go as the code begins
 months = 0
 profits = 0
-
+net_change_list = []
 
 #open and then read the csv - attemped to only open as csv, but will move to open directly into reading as a dictionary file
 with open(budget_data_csv) as csv_file:
@@ -21,15 +21,28 @@ with open(budget_data_csv) as csv_file:
     csv_header = next(csv_file)
     print(f"Header: {csv_header}")
 
+    #skip the first row since there is no change that month
+    first_row = next(csv_reader)
+
+    previous = first_row[1]
+
+
 #need to interpret the details in the csv for analysis and return the totals for months, total profits, change in profits by accreting each through a for loop
     for row in csv_reader:
         months += 1
         profits += int(row[1])
+
+        net_change = int(row[1]) - previous
+
+        previous = int(row[1])
+
+        net_change_list += [net_change]
+
         
     #Print the details in the format needed
     print(months)
-    print(profits)
-    print(profits/months)
+    print(profits) 
+    print(round(profits/months, 2))
 
 #Return the highest number in (row[1]) and zip to (row[0]) 
     #for x in range (row[1])
@@ -44,7 +57,7 @@ with open(budget_data_csv) as csv_file:
 output_path = os.path.join('Analysis', "analysis.txt")
 
 # Open the file using "write" mode. Specify the variable to hold the contents and open the file to write
-with open(output_path,"analysis.txt", 'w'):
+with open(output_path, 'w'):
     f = open(analysis.txt)
 
     #Set the Header row for the table analysis
