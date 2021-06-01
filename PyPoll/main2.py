a1549dbad85fbb65f01e79db7dc8120ef6d309d3
Jennifer Gaddie <jennifergaddie@Jennifers-MacBook-Pro.local>
2021-05-31 20:02:10 -0400

    update to code for PyPoll

--- PyPoll/main2.py
+++ PyPoll/main2.py
@@ -1,81 +1,83 @@
-# import packages to read/write CSV files and create dynamic paths to the I/O files
+#PyPoll
+
+# import packages
 import csv
 import os
 
-# -- define function to fix percentage format to 3 decimal points
+#define function to fix percentage format to 3 decimal points
 def fixPercent(num):
     num = "{:.3%}".format(num)
     return num
 
-# define relative path for the input and output files
+#create paths for both the input and output files
 inputfile = os.path.join("Resources", "election_data.csv")
 outputfile = os.path.join("Analysis", "analysis.txt")
 
-# create empty lists and variables for storing values and calculations from data
-UniqueCandidates = []
-VoteCounts = []
-VotePercent = []
-TotalVotes = 0
-WinnerCount = 0
+#create empty lists and variables for storing values and calculations from data
+unique_can = []
+tot_votes = []
+per_votes = []
+tot_votes = 0
+win_count = 0
 
 # read the CSV file
 with open(inputfile, 'r') as electiondata:
     reader = csv.reader(electiondata, delimiter=",")
 
-    # store header rows into a Headers list
+    #store header rows into a variable
     Headers = next(reader)
 
-    # for loop to go through each row in the CSV file and count the total number of votes
+    #create a for loop to go through each row in the CSV file and count the total number of votes
 
     for row in reader:
-        TotalVotes += 1
+        tot_votes += 1
     
         # get unique candidate names and individual vote counts and store in lists
-        # if row[2] (candidate name) is not in the UniqueCandidates list, i.e. if it is the first instance of the name
-        if row[2] not in UniqueCandidates:
+        # if row[2] (candidate name) is not in the unique_can list, i.e. if it is the first instance of the name
+        if row[2] not in unique_can:
 
-            #append the name to UniqueCandidates and a value of 1 to VoteCounts list
-            UniqueCandidates.append(row[2])
-            VoteCounts.append(1)
+            #append the name to unique_can and a value of 1 to tot_votes list
+            unique_can.append(row[2])
+            tot_votes.append(1)
 
-        # if row[2] (candidate name) is in the UniqueCandidates list
+        # if not,
         else:
 
-            # get the index of the candidate from the UniqueCandidates list in order to increase the vote count by 1
-            CandidateIndex = UniqueCandidates.index(row[2])
-            VoteCounts[CandidateIndex] += 1
+            #get the index of the candidate from the unique_can list and the vote count by 1
+            CandidateIndex = unique_can.index(row[2])
+            tot_votes[CandidateIndex] += 1
         
 
 # calculate percentage of votes for each candidate
-for i in range(len(VoteCounts)):
-    VotePercent.append(VoteCounts[i] / TotalVotes)
+for i in range(len(tot_votes)):
+    per_votes.append(tot_votes[i] / tot_votes)
 
 # calculate the winner based on most votes
-for i in range(len(VoteCounts)):
+for i in range(len(tot_votes)):
 
-    # if the number of votes is greater than WinnerCount (initially zero)
-    if VoteCounts[i] > WinnerCount:
+    # if the number of votes is greater than win_count (initially zero)
+    if tot_votes[i] > win_count:
         
-        #update WinnerCount to the number of votes at index i
-        WinnerCount = VoteCounts[i]
+        #update win_count to the number of votes at index i
+        win_count = tot_votes[i]
 
-        #update Winner to the candidate name at index i
-        Winner = UniqueCandidates[i]
+        #update winner to the candidate name at index i
+        winner = unique_can[i]
 
 # create a text file with the analysis output
 with open(outputfile, 'w') as textfile:
     textfile.write(f"Election Results\n"
                    f"----------------------------\n"
-                   f"Total Votes: {TotalVotes}\n"
+                   f"Total Votes: {tot_votes}\n"
                    f"----------------------------\n"
                    )
 
     # for loop to iteratively write each candidate's info
-    for i in range(len(UniqueCandidates)):
-        textfile.write(f"{UniqueCandidates[i]}: {fixPercent(VotePercent[i])} ({VoteCounts[i]})\n")
+    for i in range(len(unique_can)):
+        textfile.write(f"{unique_can[i]}: {fixPercent(per_votes[i])} ({tot_votes[i]})\n")
 
     textfile.write(f"----------------------------\n"
-                   f"Winner: {Winner}\n"
+                   f"Winner: {winner}\n"
                    f"----------------------------\n"
                   )
 
